4f548758efa8826a6b7ec4d2a0d99c18e32dca8e
SebMZI <mrgy.sebastien@gmail.com>
2026-04-19 13:36:01 +0200

    feat: setup TypeORM connection and create User entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'vestia',
+      autoLoadEntities: true,
+      synchronize: true,
+      retryAttempts: 5,
+      retryDelay: 3000,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  user_id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+  })
+  firstname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+  })
+  lastname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    unique: true,
+  })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updated_at: Date;
+}
